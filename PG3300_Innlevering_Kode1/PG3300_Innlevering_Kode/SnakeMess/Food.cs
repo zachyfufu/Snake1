2b9fc409cca7f2d8bb4801d5aa8aab71a3ef0b06
zachyfufu <zachyfufu@yahoo.com>
2017-10-12 17:18:32 +0200

    update Food class

--- PG3300_Innlevering_Kode1/PG3300_Innlevering_Kode/SnakeMess/Food.cs
+++ PG3300_Innlevering_Kode1/PG3300_Innlevering_Kode/SnakeMess/Food.cs
@@ -1,9 +1,30 @@
-﻿using System;
-
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics
+using System;
 public class Food
 {
 	public Food()
-	{
 
-	}
+	{
+        Random rnd = new Random();
+
+        pos.x = rnd.Next(0, boardW);
+        pos.y = rnd.Next(0, boardH);
+        bool spot = true;
+
+        foreach (Position i in snake)
+            if (i.x == pos.x && i.y == pos.y)
+            {
+                spot = false;
+                break;
+            }
+        if (spot)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.SetCursorPosition(pos.x, pos.y);
+            Console.Write("$");
+            break;
+        }
+    }
 }
