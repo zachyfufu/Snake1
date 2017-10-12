75fe4599cc0f6aed89f17e76d9fb082bf3d6b4d3
zachyfufu <zachyfufu@yahoo.com>
2017-10-12 18:51:39 +0200

    Revert "Merge branch 'master' of https://github.com/zachyfufu/Snake1"
    This reverts commit 7056db3be89b89c42e0e6cefa134287702e61954, reversing
    changes made to 248dc124f0ffb45dde8690d737577eadcc1f0460.

---
+++ PG3300_Innlevering_Kode1/PG3300_Innlevering_Kode/SnakeMess/Food.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SnakeMess
+{
+    class Food
+    {
+
+        public Food() { 
+
+        public void spawnFood() { 
+        Position pos = new Position();
+        Random rnd = new Random();
+
+            pos.x = rnd.Next(0, boardW);
+            pos.y = rnd.Next(0, boardH);
+            bool spot = true;
+
+            foreach (Position i in snake)
+                if (i.x == pos.x && i.y == pos.y)
+                {
+                    spot = false;
+                    break;
+                }
+                if (spot)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.SetCursorPosition(pos.x, pos.y);
+                    Console.Write("$");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ PG3300_Innlevering_Kode1/PG3300_Innlevering_Kode/SnakeMess/window.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics
+using System;
+
+public class Window
+{
+    Random rnd = new Random();
+    public Window()
+	{
+        pos.x = rnd.Next(0, boardW);
+        pos.y = rnd.Next(0, boardH);
+        bool spot = true;
+        
+
+
+    }
+}
