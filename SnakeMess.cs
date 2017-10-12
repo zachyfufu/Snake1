c5dd5ef39d1d7f155117edb5ab261236a92a6f22
kottelett <sdahle95@gmail.com>
2017-10-12 13:40:05 +0200

    mer lesbarhet

--- SnakeMess.cs
+++ SnakeMess.cs
@@ -3,25 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
 
-// WARNING: DO NOT code like this. Please. EVER! 
-//          "Aaaargh!" 
-//          "My eyes bleed!" 
-//          "I facepalmed my facepalm." 
-//          Etc.
-//          I had a lot of fun obfuscating this code! And I can now (proudly?) say that this is the uggliest short piece of code I've ever written!
-//          (And yes, it could have been ugglier. But the idea wasn't to make it fuggly-uggly, just funny-uggly or sweet-uggly.)
-//
-//          -ZACHY!!
-//
+
 namespace SnakeMess
 {
 	class Position
 	{
-		//public const string Ok = "Ok";
 
-		public int x; public int y;
-		public Position(int x = 0, int y = 0) { this.x = x; this.y = y; }
-		public Position(Position input) { x = input.x; y = input.y; }
+        public int x; public int y;
+
+		public Position(int x = 0, int y = 0)
+        {
+            this.x = x; this.y = y;
+        }
+
+        public Position(Position input)
+        {
+            x = input.x; y = input.y;
+        }
 	}
 
 	class SnakeMess
@@ -35,14 +33,13 @@ namespace SnakeMess
 			short last = newDir;
 			int boardW = Console.WindowWidth, boardH = Console.WindowHeight;
 			Random rng = new Random();
-			Position app = new Position();
+
+            Position app = new Position();
 			List<Position> snake = new List<Position>();
 			snake.Add(new Position(10, 10));
             snake.Add(new Position(10, 10));
             snake.Add(new Position(10, 10));
             snake.Add(new Position(10, 10));
-
-            // Jeg har forandret det
 
 			Console.CursorVisible = true;
 			Console.ForegroundColor = ConsoleColor.Green;
@@ -50,7 +47,8 @@ namespace SnakeMess
             Console.Write("@");
 
 			while (true) {
-				app.x = rng.Next(0, boardW); app.y = rng.Next(0, boardH);
+				app.x = rng.Next(0, boardW);
+                app.y = rng.Next(0, boardH);
 				bool spot = true;
 				foreach (Position i in snake)
 					if (i.x == app.x && i.y == app.y) {
@@ -58,7 +56,9 @@ namespace SnakeMess
 						break;
 					}
 				if (spot) {
-					Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition(app.x, app.y); Console.Write("$");
+					Console.ForegroundColor = ConsoleColor.Green;
+                    Console.SetCursorPosition(app.x, app.y);
+                    Console.Write("$");
 					break;
 				}
 			}
@@ -111,7 +111,8 @@ namespace SnakeMess
 							gg = true;
 						else {
 							while (true) {
-								app.x = rng.Next(0, boardW); app.y = rng.Next(0, boardH);
+								app.x = rng.Next(0, boardW);
+                                app.y = rng.Next(0, boardH);
 								bool found = true;
 								foreach (Position i in snake)
 									if (i.x == app.x && i.y == app.y) {
@@ -136,15 +137,20 @@ namespace SnakeMess
 					}
 					if (!gg) {
 						Console.ForegroundColor = ConsoleColor.Yellow;
-						Console.SetCursorPosition(head.x, head.y); Console.Write("0");
+						Console.SetCursorPosition(head.x, head.y);
+                        Console.Write("0");
 						if (!inUse) {
-							Console.SetCursorPosition(tail.x, tail.y); Console.Write(" ");
+							Console.SetCursorPosition(tail.x, tail.y);
+                            Console.Write(" ");
 						} else {
-							Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition(app.x, app.y); Console.Write("$");
+							Console.ForegroundColor = ConsoleColor.Green;
+                            Console.SetCursorPosition(app.x, app.y);
+                            Console.Write("$");
 							inUse = false;
 						}
 						snake.Add(newH);
-						Console.ForegroundColor = ConsoleColor.Yellow; Console.SetCursorPosition(newH.x, newH.y); Console.Write("@");
+						Console.ForegroundColor = ConsoleColor.Yellow; Console.SetCursorPosition(newH.x, newH.y);
+                        Console.Write("@");
 						last = newDir;
 					}
 				}
