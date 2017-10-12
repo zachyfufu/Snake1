e416c45c327d878a31d5eccd9623d28b893634f2
zachyfufu <zachyfufu@yahoo.com>
2017-10-12 18:54:18 +0200

    Revert "Merge branch 'master' of https://github.com/zachyfufu/Snake1"
    This reverts commit 248dc124f0ffb45dde8690d737577eadcc1f0460, reversing
    changes made to 6b85036140f7ef6735f8ce4b2dcf2e92288861ed.

--- PG3300_Innlevering_Kode1/PG3300_Innlevering_Kode/SnakeMess/SnakeMess.cs
+++ PG3300_Innlevering_Kode1/PG3300_Innlevering_Kode/SnakeMess/SnakeMess.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
 
-//
+
 namespace SnakeMess
 {
 	
@@ -16,18 +16,12 @@ namespace SnakeMess
             bool dead = false, pause = false, inUse = false;
 			short newDir = 2; // 0 = up, 1 = right, 2 = down, 3 = left
 			short last = newDir;
-<<<<<<< HEAD
            
 
            Window win = new Window();
            Position pos = new Position();
 
             
-=======
-            int boardW = Console.WindowWidth, boardH = Console.WindowHeight;
-
-			Position pos = new Position();
->>>>>>> dcc9ece889ee075e45a6547d98884623789b4218
             List<Position> snake = new List<Position>();
 			snake.Add(new Position(10, 10));
             snake.Add(new Position(10, 10));
@@ -39,6 +33,7 @@ namespace SnakeMess
             Random rnd = new Random();
 
             while (true) {
+<<<<<<< HEAD
 
                 Food food = new Food(); 
 
@@ -46,6 +41,10 @@ namespace SnakeMess
                 
 				pos.x = rnd.Next(0, boardW);
                 pos.y = rnd.Next(0, boardH);
+=======
+				pos.x = rnd.Next(0, win.BoardW);
+                pos.y = rnd.Next(0, win.BoardH);
+>>>>>>> parent of 248dc12... Merge branch 'master' of https://github.com/zachyfufu/Snake1
 				bool spot = true;
 				foreach (Position i in snake)
 					if (i.x == pos.x && i.y == pos.y) {
@@ -78,7 +77,7 @@ namespace SnakeMess
 						newDir = 3;
 				}
 				if (!pause) {
-					if (t.ElapsedMilliseconds < 100)
+					if (t.ElapsedMilliseconds < 400)
 						continue;
 					t.Restart();
 					Position tail = new Position(snake.First());
@@ -98,13 +97,13 @@ namespace SnakeMess
 							newH.x -= 1;
 							break;
 					}
-					if (newH.x < 0 || newH.x >= boardW)
+					if (newH.x < 0 || newH.x >= win.BoardW)
 						dead = true;
-					else if (newH.y < 0 || newH.y >= boardH)
+					else if (newH.y < 0 || newH.y >= win.BoardH)
 						dead = true;
 					if (newH.x == pos.x && newH.y == pos.y)
                     {
-                        if (snake.Count + 1 >= boardW * boardH)
+                        if (snake.Count + 1 >= win.BoardW * win.BoardH)
                         {
                             // No more room to place apples - game over.
                             dead = true;
@@ -113,8 +112,8 @@ namespace SnakeMess
                         {
                             while (true)
                             {
-                                pos.x = rnd.Next(0, boardW);
-                                pos.y = rnd.Next(0, boardH);
+                                pos.x = rnd.Next(0, win.BoardW);
+                                pos.y = rnd.Next(0, win.BoardH);
                                 bool found = true;
                                 foreach (Position i in snake)
                                 {
