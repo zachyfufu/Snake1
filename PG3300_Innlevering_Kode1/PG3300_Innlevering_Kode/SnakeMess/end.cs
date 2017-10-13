ccf2711411976870144c8a987127e11cf54fb6ed
zachyfufu <zachyfufu@yahoo.com>
2017-10-13 16:15:58 +0200

    end update

--- PG3300_Innlevering_Kode1/PG3300_Innlevering_Kode/SnakeMess/end.cs
+++ PG3300_Innlevering_Kode1/PG3300_Innlevering_Kode/SnakeMess/end.cs
@@ -5,7 +5,51 @@ using System.Text;
 
 namespace SnakeMess
 {
-    class Class3
+    class End 
     {
+        public void DeathOptions() 
+        {
+            Window win = new Window();
+            Position pos = new Position();
+            Random rnd = new Random();
+            Position newH = new Position();
+
+
+            if (newH.x < 0 || newH.x >= win.BoardW)
+                dead = true;
+            else if (newH.y < 0 || newH.y >= win.BoardH)
+                dead = true;
+            if (newH.x == pos.x && newH.y == pos.y)
+            {
+                if (snake.Count + 1 >= win.BoardW * win.BoardH)
+                {
+                    dead = true;
+                }
+                else
+                {
+                    while (true)
+                    {
+                        pos.x = rnd.Next(0, win.BoardW);
+                        pos.y = rnd.Next(0, win.BoardH);
+                        bool found = true;
+                        foreach (Position i in snake)
+                        {
+                            if (i.x == pos.x && i.y == pos.y)
+                            {
+                                found = false;
+                                break;
+                            }
+                        }
+                        if (found)
+                        {
+                            inUse = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+         
     }
 }
+
